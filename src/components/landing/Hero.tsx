649ce408ed78747bae20bda6becd88675dcef153
Agni133 <105123072+Agni133@users.noreply.github.com>
2026-02-28 03:02:34 +0530

    updated hero section

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+
 const Hero = () => {
   const containerVariants = {
     hidden: { opacity: 0 },
